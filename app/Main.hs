176bd1157a4727df0858ca582fef962eade3f8c8
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-11-04 23:03:06 +1100

    Actually parse more than one lexeme

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE GADTs             #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecursiveDo       #-}
 
 module Main where
 
+import           Control.Applicative      ((<|>))
 import           Control.Monad.IO.Class   (liftIO)
 import           Data.Char                (isAlpha)
 import           Data.Text                (Text)
@@ -13,49 +14,49 @@ import           System.IO                (BufferMode (..), Handle, IO, stderr,
                                            stdout)
 import qualified System.IO                as IO
 import qualified System.Process           as Process
-import           Text.Earley              ((<?>))
-import qualified Text.Earley              as E
+import           Text.Megaparsec          (Dec, ParseError, Token, sepBy)
+import qualified Text.Megaparsec          as MP
+import           Text.Megaparsec.Text     (Parser)
 
-data Command = Command String
+-- XSH would call this a `program` and that makes more semantic sense than a 'command'
+-- especially when the context is that of a script being interpreted.
+data Program where
+  Command :: Text -> [Text] -> Program
+  deriving (Show)
 
-grammar :: E.Grammar r (E.Prod r String String Command)
-grammar = mdo
-  cmd <- E.rule $ Command <$> (E.satisfy ident <?> "program")
-  return cmd
-    where ident (x:_) = isAlpha x
-          ident _     = False
+-- TODO This is actually just the parser for `Command'
+-- so best if we clarify that later via its name.
+program :: Parser Program
+program = do
+  MP.space
+  cmd <- MP.some MP.letterChar
+  MP.space
+  args <- MP.some MP.asciiChar `sepBy` MP.spaceChar -- <|> MP.space
+  return $ Command (T.pack cmd) (fmap T.pack args)
 
--- TODO `words` should be replaced with an appropriate lexer.
--- TODO Tests should ensure that the parse tree is never greater than size one (meaning it's ambiguous).
-parse line =
-  case E.fullParses (E.parser grammar) tokens of
-    -- No parse results which means nothing fit the grammar.
-    ([], report) -> error $ show report
-    (p:rst, report) ->
-      case rst of
-        [] -> p
-        -- More than one parse result, which means it's ambiguous.
-        _  -> error $ "Error: An ambiguous parse occured.\n" ++ show report
-  where
-    lex :: String -> [String]
-    lex = words
-    tokens = lex line
+{- parse :: Text -> Either (ParseError (Token Text) Dec) Program -}
+parse :: Text -> Either String Program
+parse line = case MP.parse program "" line of
+               Left _    -> Left "urk"
+               Right prg -> Right prg
 
 -- TODO Make me customisable.
 prompt :: Text
 prompt = "> "
 
-runCommand :: Handle -> Handle -> Handle -> Command -> [String] -> IO ExitCode
-runCommand i o e (Command cmd) args = do
+runCommand :: Handle -> Handle -> Handle -> Program -> IO ExitCode
+runCommand i o e (Command cmd' args') = do
   (_, _, _, h) <-
     Process.createProcess
       (Process.proc cmd args)
-      { Process.std_in = Process.UseHandle i
-      , Process.std_out = Process.UseHandle o
-      , Process.std_err = Process.UseHandle e
+      { Process.std_in    = Process.UseHandle i
+      , Process.std_out   = Process.UseHandle o
+      , Process.std_err   = Process.UseHandle e
       , Process.close_fds = True
       }
   Process.waitForProcess h
+    where cmd  = T.unpack cmd'
+          args = fmap T.unpack args'
 
 main :: IO ()
 main = do
@@ -67,8 +68,12 @@ main = do
     loop = do
       input <- Haskeline.getInputLine $ T.unpack prompt
       case input of
-        Nothing     -> pure ()
+        Nothing -> pure ()
         Just "quit" -> pure ()
-        Just line   -> do
-          liftIO $ runCommand IO.stdin IO.stdout IO.stdin (parse line) []
-          loop
+        Just line ->
+          case parse $ T.pack line of
+            Left e -> liftIO $ putStrLn e
+            Right cmd -> do
+              {- liftIO $ print cmd -}
+              liftIO $ runCommand IO.stdin IO.stdout IO.stderr cmd
+              loop
