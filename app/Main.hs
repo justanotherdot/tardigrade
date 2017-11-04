0888cd8c0bb1d190c7b073768006420db861ad29
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-11-04 15:53:14 +1100

    Accept (and echo) input from command line

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Control.Monad.IO.Class   (liftIO)
+import qualified System.Console.Haskeline as Haskeline
+
+-- TODO Make me customisable.
+prompt :: String
+prompt = "> "
 
 main :: IO ()
-main = someFunc
+main = Haskeline.runInputT Haskeline.defaultSettings loop
+  where
+    loop :: Haskeline.InputT IO ()
+    loop = do
+            input <- Haskeline.getInputLine prompt
+            case input of
+              Nothing     -> pure ()
+              Just "quit" -> pure ()
+              Just line   -> liftIO (putStrLn line) >> loop
