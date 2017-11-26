5cc2542c99aa46e014641ab134b6868be41680fa
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-11-26 12:54:39 +1100

    General touchups
    This:
    
    * Fixes the argument parser for commands in interactive mode
    * Recognizes C-d/EOF and exits accordingly
    * Recognizes 'exit' convention instead of 'quit'

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,7 @@ import qualified System.IO                as IO
 import qualified System.Process           as Process
 import           Text.Megaparsec          (Dec, ParseError, Token, sepBy)
 import qualified Text.Megaparsec          as MP
+import qualified Text.Megaparsec.Lexer    as L
 import           Text.Megaparsec.Text     (Parser)
 
 -- XSH would call this a `program` and that makes more semantic sense than a 'command'
@@ -24,26 +25,46 @@ data Program where
   Command :: Text -> [Text] -> Program
   deriving (Show)
 
+
+-- TODO This is a parser _and_ a lexer at the moment.
+-- Which is uncool; we want to separate concerns and
+-- feed a parser a stream of tokens (which will make
+-- things a lot simpler to debug).
 -- TODO This is actually just the parser for `Command'
 -- so best if we clarify that later via its name.
+-- TODO At the moment this will include quotes for
+-- tokens e.g. `-g'Main*'` whereas most shells will
+-- parse that result into `-gMain*`, indicating
+-- that the expansion takes place (whether preserving
+-- literal content with single quotes or interpolating
+-- with double quotes) and then strips the quotes from
+-- the token itself. So, perhaps it's best this happens
+-- at an 'expansion' stage, if we want that.
 program :: Parser Program
 program = do
-  MP.space
-  cmd <- MP.some MP.letterChar
-  MP.space
-  args <- MP.some MP.asciiChar `sepBy` MP.spaceChar -- <|> MP.space
-  return $ Command (T.pack cmd) (fmap T.pack args)
+    MP.space
+    cmd <- MP.some MP.alphaNumChar
+    MP.space
+    args <- MP.some flagOrOption `MP.sepEndBy` whitespace
+    return $ Command (T.pack cmd) (fmap T.pack args)
+  where
+    flagOrOption = MP.try MP.alphaNumChar <|> MP.punctuationChar
+    whitespace   = MP.some MP.spaceChar
 
 {- parse :: Text -> Either (ParseError (Token Text) Dec) Program -}
 parse :: Text -> Either String Program
 parse line = case MP.parse program "" line of
-               Left _    -> Left "Error: Could not parse provided input."
+               Left _    -> Left $ "Unable to parse input."
                Right prg -> Right prg
 
 -- TODO Make me customisable.
 prompt :: Text
 prompt = "> "
 
+-- TODO This will blow up when the passed command does not run on the path.
+-- We'll need write a wrapper anyways to determining if something needs to be
+-- run as a builtin or as an actual binary on the PATH.
+-- As such, we can capture any thrown exceptions there from createProcess.
 runCommand :: Handle -> Handle -> Handle -> Program -> IO ExitCode
 runCommand i o e (Command cmd' args') = do
   (_, _, _, h) <-
@@ -64,22 +85,27 @@ runCommand i o e (Command cmd' args') = do
     where cmd  = T.unpack cmd'
           args = fmap T.unpack args'
 
-main :: IO ()
-main = do
+-- Well typed shells don't go wrong.
+tardigrade :: IO ()
+tardigrade = do
   IO.hSetBuffering stdout LineBuffering
   IO.hSetBuffering stderr LineBuffering
   Haskeline.runInputT Haskeline.defaultSettings loop
   where
+    handleInput Nothing       = pure ()
+    handleInput (Just "exit") = pure ()
+    handleInput (Just line)   =
+          case parse $ T.pack line of
+            Left e    -> liftIO $ IO.hPutStrLn IO.stderr e
+            Right cmd -> do
+              liftIO $ print cmd
+              liftIO $ runCommand IO.stdin IO.stdout IO.stderr cmd
+              loop
+
     loop :: Haskeline.InputT IO ()
     loop = do
       input <- Haskeline.getInputLine $ T.unpack prompt
-      case input of
-        Nothing -> pure ()
-        Just "quit" -> pure ()
-        Just line ->
-          case parse $ T.pack line of
-            Left e -> liftIO $ IO.hPutStrLn IO.stderr e
-            Right cmd -> do
-              {- liftIO $ print cmd -}
-              liftIO $ runCommand IO.stdin IO.stdout IO.stderr cmd
-              loop
+      handleInput input
+
+main :: IO ()
+main = tardigrade
